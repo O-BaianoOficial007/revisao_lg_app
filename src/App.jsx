d40bb1a4c250a0051145e929192a584f7cafcfa1
O-BaianoOficial007 <arthurandradepereira7@gmail.com>
2026-07-29 12:30:02 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 
 import s from './App.module.css'
 
+import card01 from '/01.jpg'
+import card02 from '/02.jpg'
+import card03 from '/05.jpg'
+import icon01 from '/face.png'
+import icon02 from '/instagram.png'
+import icon03 from '/github.png'
+import banner from '/senai.jpg'
+
 function App() {
 
   return (
@@ -17,7 +25,7 @@ function App() {
 
         <section id='s2'>
           <div className={s.left}>
-            <img src="" alt="" />
+            <img width={400} src={banner} alt="" />
           </div>
           <div className={s.right}>
             <h2>Subtítulo</h2>
@@ -27,21 +35,21 @@ function App() {
 
         <section id='s3'>
           <div className={s.card}>
-            <img src="" alt="" />
+            <img width={400} src={card01} alt="" />
             <div className={s.cardText}>
               <h3>Título Do Card</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus enim ipsa esse iste!</p>
             </div>
           </div>
           <div className={s.card}>
-            <img src="" alt="" />
+            <img width={400} src={card02} alt="" />
             <div className={s.cardText}>
               <h3>Título Do Card</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus enim ipsa esse iste!</p>
             </div>
           </div>
           <div className={s.card}>
-            <img src="" alt="" />
+            <img width={400} src={card03} alt="" />
             <div className={s.cardText}>
               <h3>Título Do Card</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus enim ipsa esse iste!</p>
@@ -51,9 +59,9 @@ function App() {
         </section>
       </main>
       <footer>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
+        <a href=""><img src={icon01} width={40} alt="" /></a>
+        <a href=""><img src={icon02} width={40} alt="" /></a>
+        <a href=""><img src={icon03} width={40} alt="" /></a>
       </footer>
 
     </>
